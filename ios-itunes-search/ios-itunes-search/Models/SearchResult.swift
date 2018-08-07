bd8d18b194871918207a063479cf56ac6875718b
Conner Alegre <cga0@pm.me>
2018-08-07 13:43:24 -0600

    Add model

---
+++ ios-itunes-search/ios-itunes-search/Models/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  ios-itunes-search
+//
+//  Created by Conner on 8/7/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+}
