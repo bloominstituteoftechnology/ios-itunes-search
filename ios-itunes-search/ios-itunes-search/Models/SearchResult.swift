49760d18bc25e7a6ff8a00ff8f80ace9b1f33d66
Conner Alegre <cga0@pm.me>
2018-08-07 13:52:08 -0600

    SearchResult Model updated with enum for apple JSON

--- ios-itunes-search/ios-itunes-search/Models/SearchResult.swift
+++ ios-itunes-search/ios-itunes-search/Models/SearchResult.swift
@@ -11,4 +11,13 @@ import Foundation
 struct SearchResult: Codable {
     var title: String
     var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults {
+    var results: [SearchResult]
 }
