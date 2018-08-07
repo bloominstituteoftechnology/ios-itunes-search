57d48237dec9771de4fafc45fb12737f4ef415dd
stdemicheli <stdemicheli@gmail.com>
2018-08-07 22:10:08 +0200

    Added storyboards

---
+++ ios-itunes-search/ios-itunes-search/View Controllers/SearchResultsTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SearchResultsTableViewController.swift
+//  ios-itunes-search
+//
+//  Created by De MicheliStefano on 07.08.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+
+}
