e80d70524c4d549b7f227c8263cd7b85320d5e4b
Farhan Farooqui <farhan@farooqui.net>
2018-09-11 18:09:14 -0400

    project setup, enum created

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Farhan on 9/11/18.
+//  Copyright © 2018 Farhan. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
