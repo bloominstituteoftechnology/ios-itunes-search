83ba4f15bdf76b0548ddbadb02d2b9367a9ea00f
Moin Uddin <U_Moin75@yahoo.com>
2018-09-11 18:29:43 -0400

    Building Url inside performSearch function

---
+++ ios itunes search/ios itunes search/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  ios itunes search
+//
+//  Created by Moin Uddin on 9/11/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
--- ios itunes search/ios itunes search/SearchResultsController.swift
+++ ios itunes search/ios itunes search/SearchResultsController.swift
@@ -9,7 +9,57 @@
 import Foundation
 
 
+let exampleURL = "https://itunes.apple.com/search?term=eminem&entity=musicTrack"
+
+
 class SearchResultsController {
-    let baseURL = "https://itunes.apple.com/"
+    
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
+        
+        let searchUrl = baseURL.appendingPathComponent("search")
+        var components = URLComponents(url: searchUrl, resolvingAgainstBaseURL: true)
+        let searchQueryItem = URLQueryItem(name: "term", value: searchTerm)
+        components?.queryItems = [searchQueryItem]
+        
+        guard let requestURL = components?.url else {
+            NSLog("requestURL is nil")
+            completion(NSError())
+            return
+        }
+        var request = URLRequest(url: requestURL)
+        
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching data: \(error)")
+                completion(error)
+                return
+            }
+            //Turn the data into [Person]
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let termSearch = try jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults = termSearch.results
+            } catch {
+                NSLog("Unable to decode data: \(error)")
+                completion(NSError())
+                return
+            }
+        }.resume()
+    }
+    
+    
+    
+    
+    
+    let baseURL = URL(string: "https://itunes.apple.com/")!
     var searchResults: [SearchResult] = []
 }
