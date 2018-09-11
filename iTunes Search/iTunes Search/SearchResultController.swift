a80dfa70830fabb2e0f2c51183ee9d378c5f7a27
Daniela Parra <dyparra47@gmail.com>
2018-09-11 19:09:02 -0400

    Finish table view controller funcationality

--- iTunes Search/iTunes Search/SearchResultController.swift
+++ iTunes Search/iTunes Search/SearchResultController.swift
@@ -10,15 +10,16 @@ import Foundation
 
 class SearchResultController {
     
-    func performSearch(with searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> NSError?) {
+    func performSearch(with searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
         let searchURL = baseURL.appendingPathComponent("search")
         
         var components = URLComponents(url: searchURL, resolvingAgainstBaseURL: true)
         
-        let searchTermQueryItems = [
-            URLQueryItem(name: "term", value: searchTerm),
-            URLQueryItem(name: "entity", value: resultType.rawValue)
-        ]
+        let parameters: [String: String] = ["term": searchTerm, "entity": resultType.rawValue]
+        
+        let searchTermQueryItems = parameters.compactMap({
+            URLQueryItem(name: $0.key, value: $0.value)
+        })
         
         components?.queryItems = searchTermQueryItems
         
@@ -58,8 +59,6 @@ class SearchResultController {
             
             
         }.resume()
-        
-        
     }
     
     let baseURL = URL(string: "https://itunes.apple.com/")!
