8bf3e45cfdc971372316c66316a6f259fb06f4a2
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-18 14:54:13 -0500

    Initial commit. Files organized

---
+++ iTunes Search/iTunes Search/View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by Ilgar Ilyasov on 9/18/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
