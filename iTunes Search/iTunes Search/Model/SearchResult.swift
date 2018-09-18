c1ef78f1564ba3ff8ee123ca65ea2510b6a13b61
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-18 08:05:58 -0700

    created model and added JSON

---
+++ iTunes Search/iTunes Search/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Welinkton on 9/18/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult:Codable {
+    var title: String
+    var creator: String
+}
