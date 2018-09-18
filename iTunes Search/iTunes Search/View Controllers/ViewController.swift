b2ebfaa61fe8b91ad89a3ca36e93f57c435838b8
Scott Bennett <stt.btt@gmail.com>
2018-09-18 14:30:54 -0400

    Initial Commit

---
+++ iTunes Search/iTunes Search/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by Scott Bennett on 9/18/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
