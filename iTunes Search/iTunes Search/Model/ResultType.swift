d1203f4343da221eb38cb3cfc8de54ca2da68584
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-18 15:15:18 -0500

    ResultType completed

---
+++ iTunes Search/iTunes Search/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Ilgar Ilyasov on 9/18/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
