6f76710ebec327c723de602acb3cac0eabb958fd
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-18 15:13:26 -0500

    SearchResult model completed

---
+++ iTunes Search/iTunes Search/Model/SearchResult.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Ilgar Ilyasov on 9/18/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    let title: String
+    let creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case artist = "artistName"
+    }
+}
+
+struct SearchResults: Codable {
+    let results: [SearchResult]
+}
+
