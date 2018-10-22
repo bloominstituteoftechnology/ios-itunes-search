1334ba978aa7ed628e4b921224f8dbc522c9360c
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-22 18:26:17 -0400

    finished UI, search result, result type

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,16 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Yvette Zhukovsky on 10/22/18.
+//  Copyright © 2018 Yvette Zhukovsky. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+   case software
+   case musicTrack
+   case movie
+}
+
---
+++ iTunesSearch/iTunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by Yvette Zhukovsky on 10/22/18.
+//  Copyright © 2018 Yvette Zhukovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
