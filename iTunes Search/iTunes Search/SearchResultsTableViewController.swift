c04e63229cf1707e64908de98f7e4f66adb87834
Paul Yi <yipaulc@gmail.com>
2018-10-22 17:29:52 -0700

    Complete implementation of searchBarSearchButtonClicked

--- iTunes Search/iTunes Search/SearchResultsTableViewController.swift
+++ iTunes Search/iTunes Search/SearchResultsTableViewController.swift
@@ -8,16 +8,26 @@ class SearchResultsTableViewController: UITableViewController, UISearchBarDelega
         super.viewDidLoad()
         
         searchBar.delegate = self
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        // Step-by-step implementation of searchBarSearchButtonClicked
+        guard let search = searchBar.text, search.count > 0 else { return }
+        var resultType: ResultType!
+        let index = segmentControl.selectedSegmentIndex
         
+        if index == 0 {
+            resultType = .software
+        } else if index == 1 {
+            resultType = .musicTrack
+        } else {
+            resultType = .movie
+        }
+        
+        searchResultController.performSearch(searchTerm: search, resultType: resultType) {_ in
+            DispatchQueue.main.async {
+            self.tableView.reloadData()
+            }}
     }
     
     // MARK: - Table view data source
@@ -34,59 +44,9 @@ class SearchResultsTableViewController: UITableViewController, UISearchBarDelega
      
      return cell
      }
- 
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-
-
     
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var searchBar: UISearchBar!
-
 }
     
 
