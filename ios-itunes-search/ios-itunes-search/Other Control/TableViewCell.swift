9884e116e28476b3baa1cacf8438c6dbc4a8c45f
Ben Hakes <bhhakes@gmail.com>
2018-12-05 15:28:24 -0600

    set up main UI elements

---
+++ ios-itunes-search/ios-itunes-search/Other Control/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  ios-itunes-search
+//
+//  Created by Benjamin Hakes on 12/5/18.
+//  Copyright © 2018 Benjamin Hakes. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios-itunes-search/ios-itunes-search/ViewControllers/iTunesSearchTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  iTunesSearchTableViewController.swift
+//  ios-itunes-search
+//
+//  Created by Benjamin Hakes on 12/5/18.
+//  Copyright © 2018 Benjamin Hakes. All rights reserved.
+//
+
+import UIKit
+
+class iTunesSearchTableViewController: UITableViewController {
+
+    static let reuseIdentifier = "cell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: iTunesSearchTableViewController.reuseIdentifier, for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
