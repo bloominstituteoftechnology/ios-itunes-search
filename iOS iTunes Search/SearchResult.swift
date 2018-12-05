be4016f8510acf78a1c213ea339c4ac999104024
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-05 14:41:05 -0800

    add the Search Result structure

---
+++ iOS iTunes Search/SearchResult.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+//    init(title: String, creator: String) {
+//        self.title = title
+//        self.creator = creator
+//    }
+}
