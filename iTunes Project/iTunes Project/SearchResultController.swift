083e0c7a106bd71ac8f174991a7d707eb27f422d
Meera Andersen <meera.andersen@gmail.com>
2018-12-05 15:03:38 -0700

    Add baseURL and urlComponents to performSearch funciton

--- iTunes Project/iTunes Project/SearchResultController.swift
+++ iTunes Project/iTunes Project/SearchResultController.swift
@@ -3,7 +3,18 @@ import UIKit
 class SearchResultController: UITableViewController {
     static let endpoint = "https://itunes.apple.com/"
     
-    let baseURL = URL(string: endpoint)
+
     
     var searchResults: [SearchResult] = []
+    
+   static func performSearch(searchTerm: String, resultType: ResultType, completion: () -> NSError?) {
+    
+        guard let baseURL = URL(string: endpoint) else {fatalError("Unable to construct baseURL") }
+    
+        guard var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+            else {
+                fatalError("Unable to resolve baseURL to components")
+            }
+    }
+        
 }
