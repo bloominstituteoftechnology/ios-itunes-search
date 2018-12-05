6c1a310c3f07c89578b79e08f310af482c88d4bd
Frulwinn <frulwinn@gmail.com>
2018-12-05 14:02:43 -0800

    added enum in struct

--- iTunesSearch/iTunesSearch/SearchResult.swift
+++ iTunesSearch/iTunesSearch/SearchResult.swift
@@ -3,4 +3,9 @@ import Foundation
 struct SearchResult: Codable {
     let title: String
     let creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case artist = "artistName"
+    }
 }
