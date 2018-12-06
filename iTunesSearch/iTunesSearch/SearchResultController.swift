d7397522c624a2e5e03cc70b3f80f1d95be787cc
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-12-06 12:03:01 -0500

    update storyboard

---
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchResultController.swift
+//  iTunesSearch
+//
+//  Created by Julian A. Fordyce on 12/5/18.
+//  Copyright © 2018 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
