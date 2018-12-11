ada086fdc4c88baa7aee26f74c2dd680212be175
Frulwinn <frulwinn@gmail.com>
2018-12-11 14:33:05 -0800

    added tableview funcs numberOfSections and cellForRow

--- iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
+++ iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
@@ -2,9 +2,29 @@ import UIKit
 
 class SearchResultsTableViewController: UITableViewController, UISearchBarDelegate {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SearchResultController.shared.searchResults.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let searchResult = SearchResultController.shared.searchResults[indexPath.row]
+        cell.textLabel?.text = searchResult.title
+        cell.detailTextLabel?.text = searchResult.creator
+        
+        return cell
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        <#code#>
+    }
 }
 
