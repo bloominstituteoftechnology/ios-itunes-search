dee51a1d1b3e35decf3e5c68ec707e7f58f88d41
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2018-12-11 17:28:17 -0600

    Built user interface and implemented the SearchResult and SearchResultController

---
+++ iTunes Search Project - MBP/iTunes Search Project - MBP/ResultType.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+
+// Represents the kind of item we want to search for in the API
+enum ResultType: String {
+    case apps = "software"
+    case music = "musicTrack"
+    case movies = "movie"
+}
---
+++ iTunes Search Project - MBP/iTunes Search Project - MBP/SearchResult.swift
@@ -0,0 +1,19 @@
+
+import UIKit
+
+struct SearchResult: Codable {
+    
+    let title: String // JSON = artistName
+    let creator: String // JSON = trackName
+    
+    // Allow Codable to find the matching keys, despite renaming throughout application
+    enum CodingKeys: String, CodingKey {
+        case title: "trackName"
+        case creator: "artistname"
+    }
+}
+
+// Object that represents JSON at the highest level, containing resultCount and results keys
+struct TopLevelSearchResults: Codable {
+    let results: [SearchResult]
+}
