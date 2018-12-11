feed4bc432d23a346b0b9b4a34e4e7bac236f121
Vijay Das <vijaytdas@gmail.com>
2018-12-11 16:01:46 -0600

    add SearchResultsController basic file

---
+++ ios-itunes-search/ios-itunes-search/SearchResultController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResultController.swift
+//  ios-itunes-search
+//
+//  Created by Vijay Das on 12/11/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+
+    private(set) var searchResults: [SearchResult] = []
+    
+    
+    let baseURL = URL(string: "https://itunes.apple.com/search?parameterkeyvalue")
+    
+    
+    
+    
+    
+}
