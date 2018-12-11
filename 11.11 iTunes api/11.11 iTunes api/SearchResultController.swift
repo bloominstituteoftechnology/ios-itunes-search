b4686b7835132b87becf0c2f51d02ba8952dcf2a
Sean Doyle <sdoylelambda@gmail.com>
2018-12-11 17:16:24 -0500

    SearchResultController API setup completed

--- 11.11 iTunes api/11.11 iTunes api/SearchResultController.swift
+++ 11.11 iTunes api/11.11 iTunes api/SearchResultController.swift
@@ -9,7 +9,41 @@ class SearchResultController {
     
     typealias CompletionHandler = (Error?) -> Void
     
-    func performSearch(with searchTerm: String, completion: @escaping CompletionHandler ) {
+    func performSearch(with searchTerm: String, resultType: ResultType, completion: @escaping CompletionHandler) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchQueryItems = URLQueryItem(name: "term", value: searchTerm)
+        let resultTypeQueryItem = URLQueryItem(name: "entity", value: resultType.rawValue)
+        urlComponents?.queryItems = [searchQueryItems, resultTypeQueryItem]
         
-    }
+        guard let requestURL = urlComponents?.url else {
+            NSLog("Request URL is nil")
+            completion(NSError())
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+        if let error = error {
+            NSLog("Could not load JSON: \(error)")
+            completion(NSError())
+        }
+            
+            guard let data = data else {
+                NSLog("No data found")
+                completion(NSError())
+                return
+            }
+            
+            do {
+                let searchResults = try JSONDecoder().decode(ResultsList.self, from: data)
+                self.searchResults = searchResults.results
+                completion(NSError())
+            } catch {
+                NSLog("Unable to decode JSON")
+                completion(NSError())
+            }
+    } .resume()
+}
 }
