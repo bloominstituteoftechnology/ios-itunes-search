b78fa4a2c50a9cfb2ca1d13d0da69a61a2e2085e
Frulwinn <frulwinn@gmail.com>
2018-12-11 13:55:11 -0800

    added requests code to performSearch func

--- iTunesSearch/iTunesSearch/SearchResultController.swift
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -10,12 +10,23 @@ class SearchResultController {
     var searchResults: [SearchResult] = []
     
     func performSearch(with searchTerm: String, resultType: ResultType, completion: @escaping (NSError?) -> Void) {
+        
         //search
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
         let searchQueryItem = URLQueryItem(name: "term", value: searchTerm)
         let resultTypeQueryItem = URLQueryItem(name: "entity", value: resultType.rawValue)
         urlComponents?.queryItems = [searchQueryItem, resultTypeQueryItem]
-        //request
         
+        //request
+        guard let requestURL = urlComponents?.url else {
+            NSLog("error constructing search URL for \(searchTerm)")
+            completion(NSError())
+            return
+        }
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        //fetching data
+        let dataTask = URLSession
     }
 }
