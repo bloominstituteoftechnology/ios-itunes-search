343a5dbbfeb42cc32c6ffce3ba0ba57c97b39f6b
Frulwinn <frulwinn@gmail.com>
2018-12-11 13:50:35 -0800

    start performSearch func with search components

--- iTunesSearch/iTunesSearch/SearchResult.swift
+++ iTunesSearch/iTunesSearch/SearchResult.swift
@@ -3,11 +3,11 @@ import Foundation
 struct SearchResult: Codable {
     var title: String
     var creator: String
-}
 
-enum CodingKeys: String, CodingKey {
-    case title = "trackName"
-    case creator = "artistName"
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
 }
 
 //This will allow us to decode the JSON data into this object, then access the actual search results through its results property
--- iTunesSearch/iTunesSearch/SearchResultController.swift
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -1,4 +1,21 @@
 import Foundation
 
+let baseURL = URL(string: "https://itunes.apple.com/search")!
+
 class SearchResultController {
+    
+    static let shared = SearchResultController()
+    private init () {}
+    
+    var searchResults: [SearchResult] = []
+    
+    func performSearch(with searchTerm: String, resultType: ResultType, completion: @escaping (NSError?) -> Void) {
+        //search
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchQueryItem = URLQueryItem(name: "term", value: searchTerm)
+        let resultTypeQueryItem = URLQueryItem(name: "entity", value: resultType.rawValue)
+        urlComponents?.queryItems = [searchQueryItem, resultTypeQueryItem]
+        //request
+        
+    }
 }
