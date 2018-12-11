88ec07a2d2764042d0d842f700fc63bebac6a691
Sameera Leola <sameeraleola@gmail.com>
2018-12-11 17:46:31 -0600

    Completed SearchResult and SearchResultController

---
+++ iTunes Search/iTunes Search/SearchResult.swift
@@ -0,0 +1,27 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Sameera Leola on 12/11/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+}
+
+//Customize the results we want
+struct SearchResults: Codable {
+    let results: [SearchResults]
+}
+
+enum CodingKeys: String, CodingKey {
+    case title: "trackName"
+    case artist = "artistName"
+}
+
+
+
---
+++ iTunes Search/iTunes Search/SearchResultController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultController.swift
+//  iTunes Search
+//
+//  Created by Sameera Leola on 12/11/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+    
+    let baseURL = URL(string: "https://itunes.apple.com/")!
+    
+    // MARK: Data source
+    var searchResults: [SearchResult] = []
+    
+    func performSearch(with searchTerm: String, resultType: ResultType, completion: @escaping ((NSError?)-> Void) {
+        let searchURL = baseURL.appendingPathComponent("search")
+        
+        var components = URLComponents(
+    }
+}
