12bf986afdda910f4823caa6cdc1f50f15db136b
Frulwinn <frulwinn@gmail.com>
2018-12-11 12:53:40 -0800

    initial commit

--- iTunesSearch/iTunesSearch/SearchResult.swift
+++ iTunesSearch/iTunesSearch/SearchResult.swift
@@ -1,17 +1,5 @@
 import Foundation
 
 struct SearchResult: Codable {
-    
-    let title: String
-    let creator: String
-    
-    enum CodingKeys: String, CodingKey {
-    
-        case title = "trackName"
-        case creator = "artistName"
-    }
-}
-
-struct SearchResults: Codable {
-    var results: [SearchResult]
+  
 }
