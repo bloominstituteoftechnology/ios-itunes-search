290762be0803cbe54106dbe2ddfb668425387f26
Sean Doyle <sdoylelambda@gmail.com>
2018-12-12 10:40:09 -0500

    Updated TVC

--- 11.11 iTunes api/11.11 iTunes api/SearchResultsTableViewController.swift
+++ 11.11 iTunes api/11.11 iTunes api/SearchResultsTableViewController.swift
@@ -5,30 +5,27 @@ class SearchResultsTableViewController: UITableViewController, UISearchBarDelega
     @IBOutlet weak var segmentSelectorOutlet: UISegmentedControl!
     @IBOutlet weak var searchBarOutlet: UISearchBar!
     
-    //let searchResultsController: SearchResultController
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         searchBarOutlet.delegate = self
     }
-
-    // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return SearchResultController.shared.searchResults.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
+        
         let searchResult = SearchResultController.shared.searchResults[indexPath.row]
         cell.textLabel?.text = searchResult.title
         cell.detailTextLabel?.text = searchResult.creator
-
+        
         return cell
     }
     
-    func searchBarButtonClicked(_ searchBar: UISearchBar) {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let search = searchBar.text, search.count > 0 else { return }
         
         var resultType: ResultType!
