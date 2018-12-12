a2144ab402a53e839906867d06445d848c8f7161
Sameera Leola <sameeraleola@gmail.com>
2018-12-11 22:02:27 -0600

    Implemented tableView cell in SearchTableViewCell

---
+++ iTunes Search/iTunes Search/SearchTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  SearchTableViewCell.swift
+//  iTunes Search
+//
+//  Created by Sameera Leola on 12/11/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    static let reuseIdentifier =  "iTunesDataCell"
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    var serchResult: SearchResult? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        guard let searchResult = serchResult else { return }
+        
+        titleLabel.text = searchResult.title
+        detailLabel.text = searchResult.creator
+    }
+    
+    
+}
