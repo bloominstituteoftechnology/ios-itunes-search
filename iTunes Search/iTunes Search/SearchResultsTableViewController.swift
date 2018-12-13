5e298f80e26b4ad3c4f595ab01cffd8715a1c70f
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-13 13:55:58 -0800

    MVP achieved - can search for apps, songs, movies.

--- iTunes Search/iTunes Search/SearchResultsTableViewController.swift
+++ iTunes Search/iTunes Search/SearchResultsTableViewController.swift
@@ -1,95 +1,64 @@
-//
-//  SearchResultsTableViewController.swift
-//  iTunes Search
-//
-//  Created by Jaspal on 12/13/18.
-//  Copyright © 2018 Jaspal Suri. All rights reserved.
-//
-
 import UIKit
 
-class SearchResultsTableViewController: UITableViewController {
+class SearchResultsTableViewController: UITableViewController, UISearchBarDelegate {
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    let searchResultsController = SearchResultController()
+    
+    // Made a constant, despite the fact that we're only calling on it once.
+    let reuseIdentifier = "ResultCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        searchBar.delegate = self
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return searchResultsController.searchResults.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        let searchResult = searchResultsController.searchResults[indexPath.row]
+        
+        // App/Song/Movie name should be in the title.
+        cell.textLabel?.text = searchResult.title
+        // The creator/company/author name should be provided in the details.
+        cell.detailTextLabel?.text = searchResult.creator
+        
         return cell
     }
-    */
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else { return }
+        
+        let segmentIndex = segmentedControl.selectedSegmentIndex
+        
+        var resultType: ResultType!
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+        // Doesn't appear to need .rawValue() appended at the end.
+        switch segmentIndex {
+        case 0:
+            resultType = .apps
+        case 1:
+            resultType = .songs
+        default:
+            resultType = .movies
+        }
+        
+        searchResultsController.performSearch(searchTerm: searchTerm, resultType: resultType) { _, _ in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
