8a4af2a578e5ad25d6c8dce1028cb72f688f6bf5
Frulwinn <frulwinn@gmail.com>
2018-12-13 14:20:27 -0800

    set up skeleton for SearchResultsTableViewController

--- iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
+++ iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
@@ -1,9 +1,32 @@
 import UIKit
 
-class SearchResultsTableViewController: UITableViewController {
+class SearchResultsTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    let searchResultsController = SearchResultController()
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    override func viewDidLoad() {
+        <#code#>
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        <#code#>
+    }
+    
+    //tableview funcs
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    //segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
 }
 
