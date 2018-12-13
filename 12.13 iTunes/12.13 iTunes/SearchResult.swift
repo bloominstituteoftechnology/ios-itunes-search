08df44cacaa1fd05e06ed563f6e032f1289b2be7
Sean Doyle <sdoylelambda@gmail.com>
2018-12-13 15:57:15 -0500

    SearchResults.swift completed

---
+++ 12.13 iTunes/12.13 iTunes/SearchResult.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct SearchResults: Codable {
+    var title: String
+    var creator: String
+
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct ResultsList: Codable {
+    let results: [SearchResults]
+}
