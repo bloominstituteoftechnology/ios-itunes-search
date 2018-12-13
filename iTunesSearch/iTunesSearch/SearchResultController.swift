b611baba12805cea32fde9a59968ea73930a7c8e
Frulwinn <frulwinn@gmail.com>
2018-12-13 13:59:26 -0800

    added func performSearch to SearchResultController

--- iTunesSearch/iTunesSearch/SearchResultController.swift
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -9,6 +9,45 @@ class SearchResultController {
     func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (NSError?) -> Void) {
         
         //search
-        var urlComponents = 
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchQueryItem = URLQueryItem(name: "term", value: searchTerm)
+        let resultTypeQueryItem = URLQueryItem(name: "entity", value: resultType.rawValue)
+        urlComponents?.queryItems = [searchQueryItem, resultTypeQueryItem]
+        
+        //request
+        guard let requestURL = urlComponents?.url else {
+            NSLog("error constructing search URL for \(searchTerm)")
+            completion(NSError())
+            return
+        }
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        //fetching data
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("There was a problem getting data from JSON: \(error)")
+                completion(NSError())
+            }
+            guard let data = data else {
+                NSLog("No data found")
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                let searchResults = try jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults = searchResults.results
+                completion(nil)
+            } catch {
+                NSLog("error decoding JSON: \(error)")
+                completion(nil)
+                
+            }
+        }
+        dataTask.resume()
     }
 }
