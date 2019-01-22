88461a06db113e04d009e0334edab1958d0ec4a8
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-22 14:43:16 -0800

    Fixed bug that was causing no cells to show.

--- iTunes Search/iTunes Search/SearchResultsTableViewController.swift
+++ iTunes Search/iTunes Search/SearchResultsTableViewController.swift
@@ -37,7 +37,7 @@ class SearchResultsTableViewController: UITableViewController, UISearchBarDelega
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResultsController.searchResults.count
+        return results.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
