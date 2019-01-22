b33c928577df331d83774ab709085b40275222ef
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-22 13:04:51 -0800

    Build search result controller with network connection.

---
+++ iTunes Search/iTunes Search/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Nathanael Youngren on 1/22/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
