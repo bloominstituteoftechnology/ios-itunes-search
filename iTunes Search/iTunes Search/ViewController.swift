e5b5425ce37b427a97c5eb4c7765c05ac313ce39
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-22 12:36:45 -0800

    Create result model.

---
+++ iTunes Search/iTunes Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by Nathanael Youngren on 1/22/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
