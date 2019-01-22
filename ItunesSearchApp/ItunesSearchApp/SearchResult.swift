2227f8e92260ade6e68485c986151d90dae98ab1
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-22 14:49:51 -0500

    Add Model

---
+++ ItunesSearchApp/ItunesSearchApp/SearchResult.swift
@@ -0,0 +1,25 @@
+//
+//  SearchResult.swift
+//  ItunesSearchApp
+//
+//  Created by Nelson Gonzalez on 1/22/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+        
+        
+    }
+}
+
+struct SearchResults: Codable {
+    let results: [SearchResult]
+}
