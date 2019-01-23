379e08cb970c64667c7f2e9866de94642babbb4d
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-01-22 19:02:18 -0500

    create file that matches JSON in API

---
+++ iTunes Search/iTunes Search/SearchResult.swift
@@ -0,0 +1,25 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Julian A. Fordyce on 1/22/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKeys {
+        
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults: Codable {
+    let results: [SearchResult]
+}
---
+++ iTunes Search/iTunes Search/SearchResultController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchResultController.swift
+//  iTunes Search
+//
+//  Created by Julian A. Fordyce on 1/22/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
