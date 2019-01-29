b587b540f1dfad5b0c2efb50a9ccffe1b51c506c
Paul Yi <yipaulc@gmail.com>
2019-01-29 11:43:00 -0800

    Create ResultType enum

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Paul Yi on 1/29/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
