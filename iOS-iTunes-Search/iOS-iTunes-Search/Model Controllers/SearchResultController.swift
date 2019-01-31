0c9294fda2435105b2e9f5a74bf13f8ae2519f99
Vijay Das <vijaytdas@gmail.com>
2019-01-31 17:36:06 -0600

    add URL components to performSearch function

--- iOS-iTunes-Search/iOS-iTunes-Search/Model Controllers/SearchResultController.swift
+++ iOS-iTunes-Search/iOS-iTunes-Search/Model Controllers/SearchResultController.swift
@@ -10,13 +10,15 @@ import Foundation
 
 class SearchResultController {
     
-    let baseURL = URL(string: "https://itunes.apple.com/search")
+    let baseURL = URL(string: "https://itunes.apple.com/search")!
     var searchResults: [SearchResult] = []
     
-    func performSearch(with searcType: String, resultType: ResultType, completion: @escaping (Error?) -> Void?) {
+    func performSearch(with searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void?){
         
-        
-        
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
+        let urlQueryItem1 = URLQueryItem(name: "term", value: searchTerm)
+        let urlQueryItem2 = URLQueryItem(name: "entity", value: resultType.rawValue)
+        urlComponents.queryItems = [urlQueryItem1, urlQueryItem2]
         
         
     }
