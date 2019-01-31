b1ec4cfbe1a21df12ce4ff0dd1021f1d5f033c9f
Vijay Das <vijaytdas@gmail.com>
2019-01-31 17:07:24 -0600

    add Model Controller: SearchResultController and set baseURL constant

---
+++ iOS-iTunes-Search/iOS-iTunes-Search/Model Controllers/SearchResultController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultController.swift
+//  iOS-iTunes-Search
+//
+//  Created by Vijay Das on 1/31/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+    
+    let baseURL = URL(string: "https://itunes.apple.com/search")
+    
+    
+    
+    
+}
