c54b7e8beb17f97cdee501b473174ea0894781f9
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-07 16:00:45 -0700

    serachBar delegate setup

--- itunes-search/itunes-search/ViewControllers/SearchResultsTableViewController.swift
+++ itunes-search/itunes-search/ViewControllers/SearchResultsTableViewController.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 
-class SearchResultsTableViewController: UITableViewController {
+class SearchResultsTableViewController: UITableViewController, UISearchBarDelegate {
 
 	@IBOutlet var searchBar: UISearchBar!
 	@IBOutlet var segmentedControl: UISegmentedControl!
-	let searchResultsTableViewController = SearchResultsTableViewController()
+	let controller = SearchResultController()
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
+		searchBar.delegate = self
     }
-
+	
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		guard let searchTerm = searchBar.text else { return }
+		
+		print(searchTerm)
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return controller.searchResults.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+		
+		return cell
+	}
+	
 }
