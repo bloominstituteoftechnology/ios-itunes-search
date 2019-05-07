310b93e73e2798e0bc3efe059f62c159bde5e87d
Dan Morse <morsedan@outlook.com>
2019-05-07 07:51:29 -0700

    Added ResultType enum.

---
+++ iTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by morse on 5/7/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software = "software"
+    case musicTrack = "musicTrack"
+    case movie = "movie"
+}
