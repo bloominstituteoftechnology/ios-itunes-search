a33835776658519d9af86fbdaae5dd00d2b6f116
Jeffrey Carpenter <spaz926@me.com>
2019-05-07 14:38:45 -0500

    Set up custom Models and ModelController

---
+++ iTunesSearch/iTunesSearch/Models/SearchResult.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Jeffrey Carpenter on 5/7/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults: Decodable {
+    var results: [SearchResult]
+}
