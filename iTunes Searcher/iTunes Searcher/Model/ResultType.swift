91dcf30d21a1994f7794a2158d118ebf5b4b52c6
Michael Redig <mredig@gmail.com>
2019-05-07 15:12:58 -0500

    organized into folders to make nelson happy

---
+++ iTunes Searcher/iTunes Searcher/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunes Searcher
+//
+//  Created by Michael Redig on 5/7/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+	case software
+	case musicTrack
+	case movie
+}
---
+++ iTunes Searcher/iTunes Searcher/Model/SearchResult.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  iTunes Searcher
+//
+//  Created by Michael Redig on 5/7/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+import Foundation
+
+
+struct SearchResult: Decodable {
+	let title: String
+	let creator: String
+
+
+//	enum CodingKeys: String, CodingKey {
+//		case title = "trackName"
+//		case createor = "artistName"
+//	}
+}
+
+struct SearchResults {
+	let results: [SearchResult]
+}
