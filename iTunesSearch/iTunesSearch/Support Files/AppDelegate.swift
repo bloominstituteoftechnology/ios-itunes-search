472b20cae16d7e58018a4b106dc364f78fa9efcf
Jeffrey Carpenter <spaz926@me.com>
2019-05-07 14:17:19 -0500

    Create Xcode project

---
+++ iTunesSearch/iTunesSearch/Support Files/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  iTunesSearch
+//
+//  Created by Jeffrey Carpenter on 5/7/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
