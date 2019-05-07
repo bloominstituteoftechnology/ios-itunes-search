9c57d899799392709e0193a98753a3aadbdb5247
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-07 15:43:17 -0700

    viewControllerS

---
+++ itunes-search/itunes-search/ViewControllers/SearchResultsTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResultsTableViewController.swift
+//  itunes-search
+//
+//  Created by Hector Steven on 5/7/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableViewController: UITableViewController {
+
+	@IBOutlet var searchBar: UISearchBar!
+	@IBOutlet var segmentedControl: UISegmentedControl!
+	let searchResultsTableViewController = SearchResultsTableViewController()
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
