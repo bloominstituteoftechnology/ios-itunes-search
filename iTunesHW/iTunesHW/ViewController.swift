a30c90c05c4de5a28d1715edc714b409f9fb7b75
Michael R.F <mrflowers22@uwalumni.com>
2019-05-07 15:17:30 -0400

    created the model class

---
+++ iTunesHW/iTunesHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesHW
+//
+//  Created by Michael Flowers on 5/7/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
