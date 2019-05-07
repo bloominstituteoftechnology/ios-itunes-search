ae17b0e2e668b3f3de90832768ef6b9dbdb7185c
Michael Redig <mredig@gmail.com>
2019-05-07 15:15:18 -0500

    added search result controller class

---
+++ iTunes Searcher/iTunes Searcher/SearchResultController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultController.swift
+//  iTunes Searcher
+//
+//  Created by Michael Redig on 5/7/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+	let baseURL = URL(string: "https://itunes.apple.com/")
+
+	var searchResults = [SearchResult]()
+}
