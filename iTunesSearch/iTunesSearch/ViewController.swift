e559a8b2af7ae192ad04a9fabd98e643cd396bdb
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-07 16:32:01 -0400

    Create starter project

---
+++ iTunesSearch/iTunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by Lisa Sampson on 5/7/19.
+//  Copyright © 2019 Lisa M Sampson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
