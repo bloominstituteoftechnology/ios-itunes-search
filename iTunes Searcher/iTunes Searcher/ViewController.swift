984531d0823ac37bc66a50ef16e984b236cf81a4
Michael Redig <mredig@gmail.com>
2019-05-07 14:39:46 -0500

    added project

---
+++ iTunes Searcher/iTunes Searcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Searcher
+//
+//  Created by Michael Redig on 5/7/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ iTunes Searcher/iTunes SearcherTests/iTunes_SearcherTests.swift
@@ -0,0 +1,34 @@
+//
+//  iTunes_SearcherTests.swift
+//  iTunes SearcherTests
+//
+//  Created by Michael Redig on 5/7/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+import XCTest
+@testable import iTunes_Searcher
+
+class iTunes_SearcherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
