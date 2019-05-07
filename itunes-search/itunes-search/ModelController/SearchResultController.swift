567153e82ed8881b19493a0f75ab8aabd06a9eea
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-07 13:34:26 -0700

    Created Controller

---
+++ itunes-search/itunes-search/ModelController/SearchResultController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultController.swift
+//  itunes-search
+//
+//  Created by Hector Steven on 5/7/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+	let baseURL = URL(string: "https://itunes.apple.com/search?term=thestrokes")!
+}
