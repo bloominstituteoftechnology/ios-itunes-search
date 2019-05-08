3892f30e1a32f09bb98f3bf9a84d74fc78ffe34c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-07 22:47:19 -0700

    webView not able to display link from json

--- itunes-search/itunes-search/ViewControllers/WebDetaiilViewController.swift
+++ itunes-search/itunes-search/ViewControllers/WebDetaiilViewController.swift
@@ -11,30 +11,36 @@ import WebKit
 
 class WebDetaiilViewController: UIViewController, WKNavigationDelegate {
 
-	
 	override func loadView() {
 		super.loadView()
-		webView = WKWebView()
+		
+		let webconfig = WKWebViewConfiguration()
+		webView = WKWebView(frame: .zero, configuration: webconfig)
 		webView.navigationDelegate = self
 		view = webView
+		
 	}
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
 		guard let result = result else { return }
-		if let url = result.url {
-			let url = URL(string: url)!
-			webView.load(URLRequest(url: url))
+		
+		if let myurl = result.url {
+			
+			let url = URL(string: "https://www.google.com/")!
+			let myRequest = URLRequest(url: url)
+			webView.load(myRequest)
 			webView.allowsBackForwardNavigationGestures = true
+			
+			//https://itunes.apple.com/us/app/twitter/id333903271?mt=8&ign-mpt=uo%3D4
+			print(myRequest)
+		} else  {
+			print("Error : No url ")
 		}
-        
+		
     }
-    
-	var result: SearchResult? {
-		didSet{
-			print("set")
-		}
-	}
+	
+	var result: SearchResult?
 	var webView: WKWebView!
 }
