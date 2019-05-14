85f70cd8dab1fd52a1ca16b05ce003a9e14dc664
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-14 13:29:52 -0600

    Start
    Created a new Xcode project to start the iTunes Search assignment

---
+++ iTunes Search/iTunes Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by Mitchell Budge on 5/14/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
