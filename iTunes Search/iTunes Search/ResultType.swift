b57b0e2c70650ae046335dfd8c0cecbc629df989
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-14 13:39:09 -0600

    ResultType completed
    Completed and configured the ResultType.swift per the instructions.

---
+++ iTunes Search/iTunes Search/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Mitchell Budge on 5/14/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
