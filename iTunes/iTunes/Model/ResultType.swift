cf1761522f6ed70fdfd73c5be6832738c86161ac
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-14 14:55:43 -0600

    Added model files ResultsType and SearchResult. Organized files in respected files.

---
+++ iTunes/iTunes/Model/ResultType.swift
@@ -0,0 +1,17 @@
+//
+//  ResultType.swift
+//  iTunes
+//
+//  Created by Hayden Hastings on 5/14/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    
+    case software
+    case musicTrack
+    case movie
+    
+}
