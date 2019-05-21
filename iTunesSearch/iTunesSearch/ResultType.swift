f3ce5078d5f941439bb1bd3f7f1c43c4745532fe
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-21 16:01:20 -0400

    Created model, model controller, and result type files.

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,17 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Thomas Cacciatore on 5/21/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    
+    case software
+    case musicTrack
+    case movie
+    
+}
