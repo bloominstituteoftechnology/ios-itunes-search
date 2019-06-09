4e53600f7a3536e7fe776606fbf90648a1a03391
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-06-09 19:18:27 -0400

    bugs fixed and finished

--- iTunesSearch/Model Controllers/SearchResultController.swift
+++ iTunesSearch/Model Controllers/SearchResultController.swift
@@ -5,16 +5,19 @@ class SearchResultController {
     //MARK: Properties
     var searchResults: [SearchResult] = []
     
-    let baseURL = URL(string: "https://itunes.apple.com/")!
+    let baseURL = URL(string: "https://itunes.apple.com/search")!
     
     
     //MARK: Search Function
     func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
         
-        var urlCompontents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
-        let queryItems     = URLQueryItem(name: "search", value: searchTerm)
         
-        urlCompontents?.queryItems = [queryItems]
+        
+        var urlCompontents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let queryItems     = URLQueryItem(name: "term", value: searchTerm)
+        let queryItemsType = URLQueryItem(name: "entity", value: resultType.rawValue)
+    
+        urlCompontents?.queryItems = [queryItems, queryItemsType]
         
         guard let requestURL = urlCompontents?.url else {
             NSLog("URL could not be found")
@@ -24,13 +27,14 @@ class SearchResultController {
         URLSession.shared.dataTask(with: request) { (data, _, error) in
             
             if let error = error {
-                NSLog("Could not load data from URL: \(error)")
                 completion(error)
+                NSLog("Could not load data from URL: \(error)")
                 return
             }
             
             guard let data = data else {
                 completion(NSError())
+                NSLog("Could not parse data")
                 return
             }
             
@@ -45,10 +49,10 @@ class SearchResultController {
             } catch {
                 
                 completion(error)
-                
+                NSLog("Error")
             }
             
         }.resume()
-        
+        print(requestURL)
     }
 }
