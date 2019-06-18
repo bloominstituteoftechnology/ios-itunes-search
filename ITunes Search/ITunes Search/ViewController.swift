1d20253c317c309af7eb209736187819a7c03fbf
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-18 14:02:26 -0400

    Initial project setup

---
+++ ITunes Search/ITunes Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ITunes Search
+//
+//  Created by Sean Acres on 6/18/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
