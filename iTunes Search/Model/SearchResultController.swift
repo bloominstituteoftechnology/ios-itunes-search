b18989fdb1dcb90dec6062c3a8bf96ba6628a04f
Kat Milton <mltnkat@gmail.com>
2019-06-18 15:37:50 -0700

    MVP Upload.

--- iTunes Search/Model/SearchResultController.swift
+++ iTunes Search/Model/SearchResultController.swift
@@ -12,28 +12,34 @@ class SearchResultController {
     let baseURL = URL(string: "https://itunes.apple.com/search?")!
     var searchResults: [SearchResult] = []
     
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
     
-    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping() -> Void) {
-        let newURL: URL = baseURL.appendingPathComponent(resultType.rawValue)
-        var urlComponents = URLComponents(url: newURL, resolvingAgainstBaseURL: true)
-        let searchTermQueryIem = URLQueryItem(name: "search", value: searchTerm)
-        urlComponents?.queryItems = [searchTermQueryIem]
+    
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping(Error?) -> Void) {
+       
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryIem = URLQueryItem(name: "term", value: searchTerm)
+        let resultCategoryQuery = URLQueryItem(name: "entity", value: resultType.rawValue)
+        urlComponents?.queryItems = [searchTermQueryIem, resultCategoryQuery]
         guard let requestURL = urlComponents?.url else {
             print("request URL is nil")
-            completion()
             return
         }
         
         var request = URLRequest(url: requestURL)
-        request.httpMethod = resultType.rawValue
+        request.httpMethod = HTTPMethod.get.rawValue
         
-        URLSession.shared.dataTask(with: request) { data, _, error in
+        URLSession.shared.dataTask(with: request) { data, result, error in
             if let error = error {
                 print("Error fetching data: \(error)")
                 return
             }
             guard let data = data else {
-
                 print("No data returned from task.")
                 return
             }
@@ -41,11 +47,12 @@ class SearchResultController {
             do {
                 let itunesSearch = try jsonDecoder.decode(SearchResults.self, from: data)
                 self.searchResults = itunesSearch.results
-                completion()
+                completion(nil)
             } catch {
                 print("Unable to decode data into object of type [SearchResult]: \(error)")
+                completion(error)
             }
-            completion()
+            completion(nil)
         }.resume()
         
     }
