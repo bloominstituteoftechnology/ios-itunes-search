a2c4963dcc7db56ecb6002de5d26994772aae6d8
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-06 17:42:47 -0400

    Adding country code to url query

--- iTunes Search/Controllers/SearchResultController.swift
+++ iTunes Search/Controllers/SearchResultController.swift
@@ -19,6 +19,11 @@ class SearchResultController {
 		
 		urlComponents?.queryItems = [searchTermQI,entityQI]
 		
+		if let countryCode = fetchCountryCode() {
+			let countryCodeQI = URLQueryItem(name: "country", value: countryCode)
+			urlComponents?.queryItems?.append(countryCodeQI)
+		}
+		
 		guard let requestURL = urlComponents?.url else {
 			NSLog("Request URL is nil")
 			completion(NSError())
@@ -47,5 +52,9 @@ class SearchResultController {
 			}
 			completion(nil)
 		}.resume()
+	}
+	
+	private func fetchCountryCode() -> String? {
+		return Locale.current.regionCode
 	}
 }
