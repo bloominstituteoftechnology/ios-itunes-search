bf6a407eb518f9adae2f47641bea72a78ec956b6
bradleyyin <bradleyyin@gmail.com>
2019-08-06 21:22:30 -0700

    set up network calling

---
+++ itunesSearch/itunesSearch/ResultType.swift
@@ -0,0 +1,14 @@
+//
+//  ResultType.swift
+//  itunesSearch
+//
+//  Created by Bradley Yin on 8/6/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import Foundation
+enum ResultType: String{
+    case software
+    case musicTrack
+    case movie
+}
