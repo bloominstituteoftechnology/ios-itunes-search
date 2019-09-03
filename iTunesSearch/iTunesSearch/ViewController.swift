8e259a267a92ae8171aa30b6c58d8b8d61c3d684
ladybeitel <ciarabeitel@gmail.com>
2019-09-03 15:23:04 -0400

    Initial commit. Create SearchResult.swift file with structs and enum.

---
+++ iTunesSearch/iTunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by Ciara Beitel on 9/3/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
