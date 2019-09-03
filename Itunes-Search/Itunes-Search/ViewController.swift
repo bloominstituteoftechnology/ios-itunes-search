733bec5195af4b114b0213fa82b19c6489863e29
Percy <percival.ngan@gmail.com>
2019-09-03 15:30:22 -0500

    Added UI views

---
+++ Itunes-Search/Itunes-Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itunes-Search
+//
+//  Created by Percy Ngan on 9/3/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Itunes-Search/Model Controllers/SearchResult.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResult.swift
+//  Itunes-Search
+//
+//  Created by Percy Ngan on 9/3/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+
+struct SearchResult: Codable {
+
+	var title: String
+	var creator: String
+}
