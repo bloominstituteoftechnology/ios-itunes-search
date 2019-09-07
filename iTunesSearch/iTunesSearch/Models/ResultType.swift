73f503b25cc9f8cc7abbe9bd69a25ebad8dbdb0c
John Kouris <jdkouris@gmail.com>
2019-09-07 08:59:42 -0700

    Created the ResultType enum to specify the kind of item that will be searched for in the API

---
+++ iTunesSearch/iTunesSearch/Models/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by John Kouris on 9/7/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
