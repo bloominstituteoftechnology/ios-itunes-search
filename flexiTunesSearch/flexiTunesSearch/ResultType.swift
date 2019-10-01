6f3c1e102664e4f9244d97899a26217998f60642
Jerry Haaser <jerry.haaser@hotmail.com>
2019-10-01 15:11:22 -0500

    Added in models

---
+++ flexiTunesSearch/flexiTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  flexiTunesSearch
+//
+//  Created by admin on 10/1/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software = "software"
+    case musicTrack = "musicTrack"
+    case movie = "movie"
+}
---
+++ flexiTunesSearch/flexiTunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flexiTunesSearch
+//
+//  Created by admin on 10/1/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
