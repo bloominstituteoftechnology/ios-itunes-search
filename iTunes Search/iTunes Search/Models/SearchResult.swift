55c1f309c4cfaada51163f60bfb3422920d8d95b
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-10-01 09:58:23 -1000

    Finished Part 1

---
+++ iTunes Search/iTunes Search/Models/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Jesse Ruiz on 10/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults: Codable {
+    var results: [SearchResult]
+}
