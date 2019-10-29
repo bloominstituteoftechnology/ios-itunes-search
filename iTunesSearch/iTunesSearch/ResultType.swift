ac6a6759ac22436aff72c203586a3ad6fcb6dd89
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-29 13:07:36 -0700

    Added ResultType.swift file and SearchResultController.swift file code for each swift file implemented

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Lambda_School_Loaner_204 on 10/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software = "software"
+    case musicTrack = "musicTrack"
+    case movie = "movie"
+}
---
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -0,0 +1,50 @@
+//
+//  SearchResultController.swift
+//  iTunesSearch
+//
+//  Created by Lambda_School_Loaner_204 on 10/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+    let baseURL = URL(string: "https://itunes.apple.com/search?term=yelp&entity=software")!
+    var searchResults: [SearchResult] = []
+    
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "parameterkeyvalue", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        guard let requestURL = urlComponents?.url else {
+            print("Request URL is nil")
+            completion(nil)
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                print("Error fetching data: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                let searchResults = try jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults.append(contentsOf: searchResults.results)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }.resume()
+    }
+}
