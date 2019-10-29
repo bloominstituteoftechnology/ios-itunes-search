7150df4379e6284cb39e5ed20b760a601d78bf26
Jon Bash <jonbashmusic@gmail.com>
2019-10-29 12:46:49 -0700

    more SearchResult, add ResultType

--- iTunes Search/iTunes Search/SearchResult.swift
+++ iTunes Search/iTunes Search/SearchResult.swift
@@ -11,4 +11,13 @@ import Foundation
 struct SearchResult: Codable {
     var title: String
     var creator: String
+    
+    struct SearchResults {
+        let results: [SearchResult]
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
 }
