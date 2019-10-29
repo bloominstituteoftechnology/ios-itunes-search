ae160099f290c0c21326f1924b9d51756e69cfc1
Jon Bash <jonbashmusic@gmail.com>
2019-10-29 12:41:12 -0700

    add SearchResult struct

---
+++ iTunes Search/iTunes Search/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Jon Bash on 2019-10-29.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+}
