cb670d9655844d396744c9b3806299bc0b78b9b6
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-29 12:31:46 -0700

    Baseline project iTunesSearch added, added SearchResult swift file

---
+++ iTunesSearch/iTunesSearch/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Lambda_School_Loaner_204 on 10/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    let title: String
+    let creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults : Codable {
+    let results: [SearchResult]
+}
