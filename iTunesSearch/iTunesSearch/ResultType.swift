8156a80bb1b882869d3efb143f4bc06e29faf5fe
Dan M <djmthedev@gmail.com>
2019-10-29 14:09:34 -0700

    added ResultType

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by morse on 10/29/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software, musicTrack, movie
+}
+
+
