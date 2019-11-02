96338985921b94d4bb750eaa63b1875b9db97504
Joseph Rogers <joerogersss2016@gmail.com>
2019-11-01 22:37:54 -0500

    MVP

--- iTunesSearch/iTunesSearch/Controllers/SearchResultController.swift
+++ iTunesSearch/iTunesSearch/Controllers/SearchResultController.swift
@@ -21,10 +21,6 @@ class SearchResultController{
     //data source for the table view
     var searchResults: [SearchResult] = []
     
-    
-    
-    
-    
     //MARK: Networking
     
     let baseURL = URL(string: "https://itunes.apple.com/search")!
@@ -33,17 +29,12 @@ class SearchResultController{
     
     func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping () -> Void) {
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
-               let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
-               urlComponents?.queryItems = [searchTermQueryItem]
-               guard let requestURL = urlComponents?.url else {
-                   print("request URL is nil")
-                   completion()
-                   return
-               }
-               
-               var request = URLRequest(url: requestURL)
+      let parameters: [String : String] = ["term": searchTerm, "entity": resultType.rawValue]
+      let queryItems = parameters.compactMap({ URLQueryItem(name: $0.key, value: $0.value) })
+      urlComponents?.queryItems = queryItems
+      guard let requestURL = urlComponents?.url else { return }
+        var request = URLRequest(url: requestURL)
                request.httpMethod = HTTPMethod.get.rawValue
-               
                URLSession.shared.dataTask(with: request) { data, _, error in
                    if let error = error {
                        print("Error fetching data: \(error)")
@@ -53,12 +44,11 @@ class SearchResultController{
                        print("No data returned from data Task")
                        return
                    }
-
                    let jsonDecoder = JSONDecoder()
                    jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
                    do{
-                    let iTunesSearch = try jsonDecoder.decode(SearchResult.SearchResults.self, from: data)
-                    self.searchResults.append(contentsOf: iTunesSearch.results)
+                    let iTunesSearch = try jsonDecoder.decode(SearchResults.self, from: data)
+                    self.searchResults = iTunesSearch.results
                    } catch {
                        print("Unable to decode data into object of type [SearchResults]: \(error)")
                    }
