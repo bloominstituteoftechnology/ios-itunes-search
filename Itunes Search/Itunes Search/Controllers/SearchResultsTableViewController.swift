cb30ff0177fcd9eb99960c070096f195b32bfa79
Nicolas Rios <nickrios33@yahoo.com>
2019-11-03 18:56:53 -0800

    added viewcontroller and navigation controller

--- Itunes Search/Itunes Search/Controllers/SearchResultsTableViewController.swift
+++ Itunes Search/Itunes Search/Controllers/SearchResultsTableViewController.swift
@@ -15,20 +15,17 @@ class SearchResultsTableViewController: UITableViewController {
     
     
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+       
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
