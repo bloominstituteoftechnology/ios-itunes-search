e634f57ed2a954af4f3af5343889f7bfd5f5baf5
Zack Larsen <zacklarsen1@gmail.com>
2019-12-03 14:58:11 -0700

    Part 2 finished...

--- ItunesSearch/ItunesSearch/SearchResultController.swift
+++ ItunesSearch/ItunesSearch/SearchResultController.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+class SearchResultController {
+    let baseURL = URL(string: "https://itunes.apple.com/search?parameterkeyvalue")!
+
+    var searchResults: [SearchResult] = []
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping () -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        guard let requestURL = urlComponents?.url else {
+            print ("request URL is nil")
+            completion()
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                print("Error fetching data: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print ("No data returned from data task.")
+                return
+            }
+            let jsonDecoder = JSONDecoder()
+            do {
+                let searchResult = try
+                    jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults.append(contentsOf: searchResult.results)
+            } catch {
+                print("Unable to decode data into object of type [SearchResult]: \(error)")
+                
+            }
+            DispatchQueue.main.async {
+                completion()
+            }
+        }.resume()
+    }
+}
