6b1cdc831773b281bdd5ee9c41b749652c40da0b
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-03 16:08:41 -0500

    Completed Network Logic

---
+++ iTunesSearch/iTunesSearch/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Chad Rutherford on 12/3/19.
+//  Copyright © 2019 chadarutherford.com. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software = "software"
+    case musicTrack = "musicTrack"
+    case movie = "movie"
+}
---
+++ iTunesSearch/iTunesSearch/Model/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Chad Rutherford on 12/3/19.
+//  Copyright © 2019 chadarutherford.com. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    let title: String
+    let creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults: Codable {
+    let results: [SearchResult]
+}
