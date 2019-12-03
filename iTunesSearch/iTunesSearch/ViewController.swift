40fd523f45bde1318cd735a110136b869c14c514
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-03 15:25:51 -0500

    Initial Commit

---
+++ iTunesSearch/iTunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by Chad Rutherford on 12/3/19.
+//  Copyright © 2019 chadarutherford.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
