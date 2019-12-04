3cc919b931e024cdfe0a7be462c45913a91aa5eb
Zack Larsen <zacklarsen1@gmail.com>
2019-12-03 19:34:35 -0700

    Finished the app and it works

--- ItunesSearch/ItunesSearch/SearchResultController.swift
+++ ItunesSearch/ItunesSearch/SearchResultController.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class SearchResultController {
-    let baseURL = URL(string: "https://itunes.apple.com/search?parameterkeyvalue")!
+    let baseURL = URL(string: "https://itunes.apple.com/search")!
 
     var searchResults: [SearchResult] = []
     
@@ -23,14 +23,13 @@ class SearchResultController {
     func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
         let searchTermQueryItem = URLQueryItem(name: "term", value: searchTerm)
-        let searchTermQueryItem = URLQueryItem(name: "entity", value: searchTerm)
-        urlComponents?.queryItems = [searchTermQueryItem]
+        let searchEntityQueryItem = URLQueryItem(name: "entity", value: resultType.rawValue)
+        urlComponents?.queryItems = [searchTermQueryItem, searchEntityQueryItem]
         guard let requestURL = urlComponents?.url else {
             print ("request URL is nil")
-            completion()
             return
         }
-        
+        print(requestURL)
         var request = URLRequest(url: requestURL)
         request.httpMethod = HTTPMethod.get.rawValue
         URLSession.shared.dataTask(with: request) { data, _, error in
@@ -41,19 +40,21 @@ class SearchResultController {
             
             guard let data = data else {
                 print ("No data returned from data task.")
+                completion(nil)
                 return
             }
             let jsonDecoder = JSONDecoder()
             do {
                 let searchResult = try
                     jsonDecoder.decode(SearchResults.self, from: data)
-                self.searchResults.append(contentsOf: searchResult.results)
+                self.searchResults = searchResult.results
+                print (self.searchResults)
             } catch {
                 print("Unable to decode data into object of type [SearchResult]: \(error)")
                 
             }
             DispatchQueue.main.async {
-                completion()
+                completion(nil)
             }
         }.resume()
     }
--- ItunesSearch/ItunesSearch/SearchResultsTableViewController.swift
+++ ItunesSearch/ItunesSearch/SearchResultsTableViewController.swift
@@ -17,41 +17,53 @@ class SearchResultsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         searchBar.delegate = self
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return searchResultsController.searchResults.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         // Configure the cell...
-       let searchResult = searchResultsController.searchResults[indexPath.row]
+        let searchResult = searchResultsController.searchResults[indexPath.row]
         
         cell.textLabel?.text = searchResult.title
         cell.detailTextLabel?.text = searchResult.creator
-
+        
+      return cell
     }
     
 }
-    extension SearchResultsTableViewController: UISearchBarDelegate {
-        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) { guard let searchTerm = searchBar.text else { return }
-            
-        
-            var resultType: ResultType!
-    
-        searchBarSearchButtonClicked()
-            switch resultType {
-            case "Apps":
-                selectedSegmentIndex
-            case "Music":
-            case "Movies":
+extension SearchResultsTableViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        guard let searchTerm = searchBar.text else { return }
+        var resultType: ResultType!
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            resultType = .software
+        case 1:
+            resultType = .musicTrack
+        case 2:
+            resultType = .movie
+        default:
+            break
+        }
+        searchResultsController.performSearch(searchTerm: searchTerm, resultType: resultType) { (error) in
+            if let error = error {
+                print("Error performing search: \(error)")
+                return
             }
-        
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-
+    
+    
 }
