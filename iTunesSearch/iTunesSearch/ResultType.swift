9aa4bfb495524159791779d961ac68ef16dd1148
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-14 11:55:04 -0800

    Part 1 half done

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Jorge Alvarez on 1/14/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software = "software"
+    case musicTrack = "musicTrack"
+    case movie = "movie"
+}
---
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultController.swift
+//  iTunesSearch
+//
+//  Created by Jorge Alvarez on 1/14/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+    
+}
