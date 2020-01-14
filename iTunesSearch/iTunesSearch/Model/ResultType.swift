3616cdd3b0462a1665354b576dec096fc1c14228
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-14 21:24:43 +0000

    Models created

---
+++ iTunesSearch/iTunesSearch/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Tobi Kuyoro on 14/01/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+enum ResulType: String {
+    case software
+    case musicTrack
+    case movie
+}
