c85b9611eb36dcbb39be39e3a7965b6c25cecc72
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-14 13:43:25 -0800

    Part 3 almost done

--- iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
+++ iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
@@ -13,23 +13,57 @@ class SearchResultsTableViewController: UITableViewController {
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    
+    let searchResultsController = SearchResultController()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchBar.delegate = self
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return searchResultsController.searchResults.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath)
-
+        
+        let result = searchResultsController.searchResults[indexPath.row]
+        cell.textLabel?.text = result.title
+        cell.detailTextLabel?.text = result.creator
 
         return cell
     }
 }
+
+/* ??? Error Handling
+4. Call the performSearch method of the searchResultsController. Pass in the search term, and the resultType. In the completion closure of this method, check for errors. If there is no error, reload the table view on the correct queue.
+*/
+
+extension SearchResultsTableViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text else {return}
+        
+        var resultType: ResultType!
+        
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            resultType = .software
+        case 1:
+            resultType = .musicTrack
+        case 2:
+            resultType = .movie
+        default:
+            return // ? or break? or .softare?
+        }
+        
+        searchResultsController.performSearch(searchTerm: searchTerm, resultType: resultType) {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+}
+
+
