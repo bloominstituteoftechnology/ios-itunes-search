3832b757ff6c5d58683d9065f4e43a8cf5a54b55
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-01-17 18:41:14 -0500

    Added SearchResult model

---
+++ iTunes Search/iTunes Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by Joshua Rutkowski on 1/17/20.
+//  Copyright © 2020 Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
