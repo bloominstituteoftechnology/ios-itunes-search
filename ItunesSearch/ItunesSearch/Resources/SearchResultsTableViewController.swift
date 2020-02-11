8d691c8528a9b55a2834c3f9021b6d5222dcc9c9
scottharris86 <scottharrisdev@gmail.com>
2020-02-11 18:29:04 -0500

    created search results table view controller laid out and connected ui in storyboard

---
+++ ItunesSearch/ItunesSearch/Resources/SearchResultsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultsTableViewController.swift
+//  ItunesSearch
+//
+//  Created by scott harris on 2/11/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableViewController: UITableViewController {
+    @IBOutlet weak var resultTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+}
