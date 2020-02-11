fc311819ad357d40d1e421cc30ee0273f6b57358
Chris <chris.gonzales.82@gmail.com>
2020-02-11 12:42:06 -0800

    ResutlType struct created

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,16 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Chris Gonzales on 2/11/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
+
+
+enum ResultType: String{
+    case software
+    case musicTrack
+    case movie
+}
