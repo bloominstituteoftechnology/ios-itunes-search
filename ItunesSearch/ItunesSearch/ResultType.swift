d1f6da1596d11f44856cf0a962c0e83ff387b820
Keri Levesque <kerianne166@gmail.com>
2020-02-11 18:25:51 -0500

    completed storyboard and set up table view controller file

---
+++ ItunesSearch/ItunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  ItunesSearch
+//
+//  Created by Keri Levesque on 2/11/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import Foundation
+// represents the kind of item wanted from the API
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
