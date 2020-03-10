1b680827341a79255ecb7f7948ec818678e80f25
Nichole Davidson <nicholedavid@gmail.com>
2020-03-10 14:03:03 -0600

    Initial setup with SearchResult structs

---
+++ iTunesSearch/iTunesSearch/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Nichole Davidson on 3/10/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults {
+    let results: [SearchResult]
+}
