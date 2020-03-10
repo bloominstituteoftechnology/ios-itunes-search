3180a46ec194fc4235494b8634757e9979cd0961
Nichole Davidson <nicholedavid@gmail.com>
2020-03-10 15:42:26 -0600

    Added SearchResultController code

--- iTunesSearch/iTunesSearch/Model Controller/SearchResultController.swift
+++ iTunesSearch/iTunesSearch/Model Controller/SearchResultController.swift
@@ -9,8 +9,45 @@
 import Foundation
 
 class SearchResultController {
-    let baseURL = URL(string: "https://itunes.apple.com/search?term")
+    let baseURL = URL(string: "https://itunes.apple.com/search")!
     
     //data source for table view
     var searchResults: [SearchResult] = []
+    
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "term", value: searchTerm)
+        let searchTermQueryItem2 = URLQueryItem(name: "entity", value: resultType.rawValue)
+        urlComponents?.queryItems = [searchTermQueryItem, searchTermQueryItem2]
+        guard let requestURL = urlComponents?.url else {
+            NSLog("request URL is nil")
+            return
+        }
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching data: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task.")
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                let itunesSearch = try jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults.append(contentsOf: itunesSearch.results)
+                completion(nil)
+                
+            } catch {
+                completion(error)
+            }
+      }.resume()
+    }
 }
