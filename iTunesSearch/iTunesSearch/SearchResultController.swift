ffe43d549a54999548a6d229d4d35b6887104d67
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-10 15:42:55 -0400

    part 1 complete

---
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -0,0 +1,54 @@
+//
+//  SearchResultController.swift
+//  iTunesSearch
+//
+//  Created by Wyatt Harrell on 3/10/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+    
+    var searchResults: [SearchResult] = []
+    private let baseURL = URL(string: "https://itunes.apple.com/")!
+    
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            NSLog("requestURL is nil")
+            completion(nil)
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching data: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data retunred from data task.")
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                let itunesSearch = try jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults.append(contentsOf: itunesSearch.results)
+                completion(nil)
+            } catch {
+                NSLog("Unable to decode data into object \(error)")
+                completion(error)
+            }
+        }.resume()
+    }
+}
