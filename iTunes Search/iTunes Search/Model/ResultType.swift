5b5c93c40256bba32937c205437808e01d6e4980
Mark Gerrior <github@gerrior.com>
2020-03-10 14:36:38 -0700

    Added ResultType

---
+++ iTunes Search/iTunes Search/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Mark Gerrior on 3/10/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
