b446c3b70494f84ce11d0b4df5644d2383d42ac2
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-10 15:19:11 -0400

    basic setup

---
+++ iTunesSearch/iTunesSearch/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Wyatt Harrell on 3/10/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    let title: String
+    let creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults: Codable {
+    var results: [SearchResult]
+}
