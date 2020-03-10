b6610c5ba67f87b4a16e214783bae66a8cebf9da
Mark Gerrior <github@gerrior.com>
2020-03-10 15:46:35 -0700

    Added SearchResultController

---
+++ iTunes Search/iTunes Search/Model/SearchResultController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultController.swift
+//  iTunes Search
+//
+//  Created by Mark Gerrior on 3/10/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResultController {
+    
+    private let baseURL = URL(string: "https://itunes.apple.com/search")!
+
+}
