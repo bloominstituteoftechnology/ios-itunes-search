a8b676ddb27ca7edc96f9ab4ef724fd10cd1d69f
Brad <braddiroff@icloud.com>
2020-03-10 18:02:17 -0400

    completed w/ 1 stretch goal

---
+++ iTunesStoreSearch/iTunesStoreSearch/Models/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesStoreSearch
+//
+//  Created by Bradley Diroff on 3/10/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
