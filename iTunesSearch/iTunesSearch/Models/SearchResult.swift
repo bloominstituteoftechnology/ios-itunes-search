acf83c363899837d168654257cf15faa7fdf2cf8
Hector L <hectoremil21@hotmail.com>
2020-03-10 15:15:55 -0400

    Create Result Model. Resuts Model. and Type enum.

---
+++ iTunesSearch/iTunesSearch/Models/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Karen Rodriguez on 3/10/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults: Codable {
+    var results: [SearchResult]
+}
