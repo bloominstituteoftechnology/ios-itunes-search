2f24b1bf02aa1df4baec84bd9c00fea22c7f8977
Libby Thomas <thomaslibby05@gmail.com>
2020-03-13 18:53:38 -0500

    Organized files and set up views

---
+++ iOS iTunes Search/iOS iTunes Search/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  iOS iTunes Search
+//
+//  Created by Elizabeth Thomas on 3/13/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+}
