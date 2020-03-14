2e148c5ff6fb0b36dddb740c268ff22b9ac3e0a2
Chad Parker <chad@chadparker.com>
2020-03-13 20:50:42 -0700

    Implement TableView delegate methods

--- iTunes Search/iTunes Search/View Controllers/SearchResultsTableViewController.swift
+++ iTunes Search/iTunes Search/View Controllers/SearchResultsTableViewController.swift
@@ -13,6 +13,8 @@ class SearchResultsTableViewController: UITableViewController {
     @IBOutlet weak var resultTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    private let searchResultsController = SearchResultController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,12 +22,14 @@ class SearchResultsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return searchResultsController.searchResults.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath)
+        let result = searchResultsController.searchResults[indexPath.row]
+        cell.textLabel?.text = result.title
+        cell.detailTextLabel?.text = result.creator
         return cell
     }
 }
