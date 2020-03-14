3a1d13f144febdb386964a21604e4433b434caa8
Matt Martindale <martindalematt18@yahoo.com>
2020-03-14 12:36:57 -0600

    stretch goal - update search with new media selection

--- itunesSearch/itunesSearch/View Controller/SearchResultsTableViewController.swift
+++ itunesSearch/itunesSearch/View Controller/SearchResultsTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class SearchResultsTableViewController: UITableViewController {
     
     let searchResultController = SearchResultController()
-
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
@@ -20,46 +20,55 @@ class SearchResultsTableViewController: UITableViewController {
         searchBar.delegate = self
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchResultController.searchResults.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath)
         let searchIndex = searchResultController.searchResults[indexPath.row]
         cell.textLabel?.text = searchIndex.title
         cell.detailTextLabel?.text = searchIndex.creator
-
+        
         return cell
+    }
+    
+    @IBAction func changedSearchParams(_ sender: Any) {
+        searchResultController.searchResults = []
+        updateWithNewMediaSearch()
+    }
+    
+    func updateWithNewMediaSearch() {
+        guard let searchTerm = searchBar.text,
+            searchTerm != "" else { return }
+        
+        var resultType: ResultType!
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            resultType = .software
+        case 1:
+            resultType = .musicTrack
+        case 2:
+            resultType = .movie
+        default:
+            break
+        }
+        searchResultController.performSearch(searchTerm: searchTerm, resultType: resultType) {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
 }
 
 extension SearchResultsTableViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let searchTerm = searchBar.text,
-            searchTerm != "" else { return }
-        
-            var resultType: ResultType!
-        
-            switch segmentedControl.selectedSegmentIndex {
-            case 0:
-                resultType = .software
-            case 1:
-                resultType = .musicTrack
-            case 2:
-                resultType = .movie
-            default:
-                break
-            }
-            searchResultController.performSearch(searchTerm: searchTerm, resultType: resultType) {
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            }
-        }
+        updateWithNewMediaSearch()
     }
+}
 
