41ad567f34d2569ba2beed0b61a9762869368f22
Matt Martindale <martindalematt18@yahoo.com>
2020-03-14 12:27:15 -0600

    finished project

--- itunesSearch/itunesSearch/Model/SearchResultController.swift
+++ itunesSearch/itunesSearch/Model/SearchResultController.swift
@@ -21,48 +21,37 @@ class SearchResultController {
     
     var searchResults: [SearchResult] = []
     
-    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping () -> Void) {
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
-        let searchTermQueryItem = URLQueryItem(name: "term", value: searchTerm)
-        urlComponents?.queryItems = [searchTermQueryItem]
+        let parameters: [String: String] = ["term": searchTerm, "entity": resultType.rawValue]
+        let queryItems = parameters.compactMap({ URLQueryItem(name: $0.key, value: $0.value) })
+        urlComponents?.queryItems = queryItems
         
-        guard let requestURL = urlComponents?.url else {
-                print("Error: Request URL is nil")
-            // TODO: - Completion handler
-            completion(nil)
-                return
-        }
+        guard let requestURL = urlComponents?.url else { return }
         var request = URLRequest(url: requestURL)
         request.httpMethod = HTTPMethod.get.rawValue
         
+        
+        
         URLSession.shared.dataTask(with: request) { (data, _, error) in
             
-            let apps = ResultType.software
-            let music = ResultType.musicTrack
-            let movie = ResultType.movie
-            
-            guard error == nil else {
-                print("Error fetching data: \(error!)")
-                completion(error)
-                return
+            if let error = error {
+                NSLog("Wrror fetching data: \(error)")
             }
             
-            guard let data = data else {
-                print("Error: no data returned from data task")
-                completion(error)
-                return
-            }
+            guard let data = data else { completion(); return }
             
             let jsonDecoder = JSONDecoder()
+            
             do {
                 let searchResults = try jsonDecoder.decode(SearchResults.self, from: data)
-                self.searchResults.append(contentsOf: searchResults.results)
-                completion(nil)
+                self.searchResults = searchResults.results
             } catch {
                 print("Unable to decode data: \(error)")
-                completion(error)
             }
+            
+            completion()
+            
         }.resume()
     }
-    
 }
