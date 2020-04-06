4c3f3ff89380e2d9953b68167e8a4434b7743b2b
Shawn-James <shawn.james@me.com>
2020-04-06 17:11:30 -0400

    add ResultType file

---
+++ iTunesSearch/iTunesSearch/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Shawn James on 4/6/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
