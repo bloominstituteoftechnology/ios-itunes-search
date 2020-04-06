42e8f1b193f65cb80776e070d9d9be91b254a305
Harmony Radley <harmonyradley99@gmail.com>
2020-04-06 17:18:09 -0400

    Created SearchResult.swift model with struct

---
+++ iTunes Search/iTunes Search/Model/SearchResult.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Harmony Radley on 4/6/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    
+    var title: String
+    var creator: String
+    
+    
+    
+    
+}
---
+++ iTunes Search/iTunes Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by Harmony Radley on 4/6/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
