1285120363515c070d80b92d17200b454328e6e0
Shawn-James <shawn.james@me.com>
2020-04-06 17:08:26 -0400

    add search result file

--- iTunesSearch/iTunesSearch/Model/SearchResult.swift
+++ iTunesSearch/iTunesSearch/Model/SearchResult.swift
@@ -8,4 +8,16 @@
 
 import Foundation
 
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
 
+struct SearchResults {
+    let results = [SearchResult]()
+}
