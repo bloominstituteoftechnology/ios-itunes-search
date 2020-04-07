46a237e9d2f62ed7d2617cc35c762090f46c04ec
Harmony Radley <harmonyradley99@gmail.com>
2020-04-06 21:40:28 -0400

    Finished part 1 almost...

--- iTunesSearch/iTunesSearch/SearchResultController.swift
+++ iTunesSearch/iTunesSearch/SearchResultController.swift
@@ -10,13 +10,64 @@ import Foundation
 
 class SearchResultController {
     
-    private let baseURL = URL(string: "https://itunes.apple.com/search")
+    enum HTTPMethod: String{
+        
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
     
-    private var searchResults: [SearchResult] = []
-    
+    var searchResults: [SearchResult] = []
+    private let baseURL = URL(string: "https://itunes.apple.com/search")!
     private var task: URLSessionTask?
     
-    func performSearch(searchTerm: String, resultType: ResultType, completion: Error?  -> Void) {
+    func performSearch(searchTerm: String, resultType: ResultType, completion: (Error?) -> Void) {
+        task?.cancel()
+        
+        // create request
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchQueryItem]
+        guard let requestURL = urlComponents?.url else {
+            print("Request URL is nil")
+            completion(nil)
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        // Create data task
+        
+        task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            if let error = error {
+                print("Error fetching data: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let self = self else { return }
+            guard let data = data else {
+                print("No data returned from dataTask")
+                return
+            }
+            
+            // Decoding
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let searchResults = try jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults = searchResults.results
+            } catch {
+                print("Unable to decode data into instance of SearchResults: \(error.localizedDescription)")
+                
+            }
+            
+            completion(nil)
+        }
+        
+        task?.resume()
         
     }
 }
