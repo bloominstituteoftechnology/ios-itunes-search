72100c55139987a0647bbcdfcabb0c5e847ff95e
Nihal Erdal <nihalerdall@gmail.com>
2020-04-06 17:59:50 -0700

    part 1

---
+++ iTunesSearch/iTunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by Nihal Erdal on 4/6/20.
+//  Copyright © 2020 Nihal Erdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
