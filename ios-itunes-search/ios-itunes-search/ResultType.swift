bbfbbcba2e93dad5572a4619fe9c02bef0d73804
ahavamorse <ahavamorse@icloud.com>
2020-05-01 10:10:02 -0700

    Created ResultType Enum

---
+++ ios-itunes-search/ios-itunes-search/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  ios-itunes-search
+//
+//  Created by Ahava on 5/1/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software // for apps
+    case musicTrack
+    case movie
+}
