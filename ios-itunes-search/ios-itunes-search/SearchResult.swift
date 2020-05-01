dad7c76e5f379298021e7f956366e77245a0977b
ahavamorse <ahavamorse@icloud.com>
2020-05-01 10:08:24 -0700

    Created SearchResult struct and SearchResults struct

---
+++ ios-itunes-search/ios-itunes-search/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  ios-itunes-search
+//
+//  Created by Ahava on 5/1/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+}
+
+struct SearchResults {
+    let results: [SearchResult]
+}
+
+enum CodingKeys: String, CodingKey {
+    case title = "trackName"
+    case creator = "artistName"
+}
---
+++ ios-itunes-search/ios-itunes-search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-itunes-search
+//
+//  Created by Ahava on 5/1/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
