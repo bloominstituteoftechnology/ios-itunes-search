c824ae2f92b397a8ffae87795143169e3b2d1227
Bronson <bronsonmullens@icloud.com>
2020-05-06 16:40:57 -0700

    Created SearchResult.swift

---
+++ iTunes Search/iTunes Search/ResultType.swift
@@ -0,0 +1,9 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Bronson Mullens on 5/6/20.
+//  Copyright © 2020 Bronson Mullens. All rights reserved.
+//
+
+import Foundation
---
+++ iTunes Search/iTunes Search/SearchResult.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by Bronson Mullens on 5/6/20.
+//  Copyright © 2020 Bronson Mullens. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+    /* A class/struct that adopts Codable will use the names
+    of its properties as keys it should look for in the JSON
+    file it is trying to decode. CodingKeys overrides this
+    behavior.
+    */
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults {
+    let results: [SearchResult]
+}
