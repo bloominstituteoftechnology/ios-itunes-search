667477118b6246d74a89e137bd01c96881e3327f
Robert Vance <Fearmydotzs@gmail.com>
2020-05-06 21:27:18 -0600

    Issues with showing results

---
+++ iTunesSearch/iTunesSearch/Views/iTunesSearchTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchTableViewCell.swift
+//  iTunesSearch
+//
+//  Created by Rob Vance on 5/6/20.
+//  Copyright © 2020 Robs Creations. All rights reserved.
+//
+
+//import UIKit
+//
+//class iTunesSearchTableViewCell: UITableViewCell {
+//    // Mark: IBOutlets
+//
+//    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var subtitleLabel: UILabel!
+//
+//}
