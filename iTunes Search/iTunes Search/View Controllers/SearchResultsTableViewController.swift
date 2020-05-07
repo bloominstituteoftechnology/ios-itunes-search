8a78987a5dcd5f85ce3f2a2be3e5bca5ee546472
Bronson <bronsonmullens@icloud.com>
2020-05-06 19:10:08 -0700

    Error when searching store

---
+++ iTunes Search/iTunes Search/View Controllers/SearchResultsTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SearchResultsTableViewController.swift
+//  iTunes Search
+//
+//  Created by Bronson Mullens on 5/6/20.
+//  Copyright © 2020 Bronson Mullens. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    let searchResultController = SearchResultController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        guard let searchTerm = searchBar.text else { return }
+        var resultType: ResultType!
+        
+        switch categorySegmented.selectedSegmentIndex {
+        case 0:
+            resultType = .software
+        case 1:
+            resultType = .musicTrack
+        case 2:
+            resultType = .movie
+        default:
+            break
+        }
+        
+        searchResultController.performSearch(searchTerm: searchTerm, resultType: resultType) { _ in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResultController.searchResults.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let searchResultCell = searchResultController.searchResults[indexPath.row]
+        cell.textLabel?.text = searchResultCell.title
+        cell.detailTextLabel?.text = searchResultCell.creator
+
+        return cell
+    }
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var categorySegmented: UISegmentedControl!
+    @IBOutlet weak var searchBar: UISearchBar!
+}
