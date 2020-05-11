01f2312b138fa6856b145146cbb804a7c557c929
Aaron Peterson <aarondp2011@gmail.com>
2020-05-10 19:22:55 -0500

    Set up url request method

---
+++ iTunesSearch/iTunesSearch/Models/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Aaron Peterson on 5/6/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
