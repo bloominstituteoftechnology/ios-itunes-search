888ac9ee1ae54da7f7a9ade1ee8973c90cbbb03e
Sammy Alvarado <isammyalvarado@gmail.com>
2020-07-08 20:06:27 -0400

    Added enum ResultType
    Added new enum ResultType this will allow us to search for items we want in the API

---
+++ iTunes Search/iTunes Search/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Sammy Alvarado on 7/8/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
