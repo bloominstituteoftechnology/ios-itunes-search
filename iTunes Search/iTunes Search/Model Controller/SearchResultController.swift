e5a36200633a33a946311482fecf53d7e8f30112
Sammy Alvarado <isammyalvarado@gmail.com>
2020-07-09 00:16:34 -0400

    Updated SearchResultController
    Added new enum HTTPMethod with raw value Get to pull information from API.

--- iTunes Search/iTunes Search/Model Controller/SearchResultController.swift
+++ iTunes Search/iTunes Search/Model Controller/SearchResultController.swift
@@ -10,13 +10,53 @@ import Foundation
 
 class SearchResultController {
 
+    enum HTTPMethod: String {
+        case get = "Get"
+    }
+
     private let baseURL = URL(string: "https://itunes.apple.com/search")!
     var searchResults: [SearchResult] = []
 
     func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (Error?) -> Void) {
+
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItems = URLQueryItem(name: "term", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItems]
+//        let error = NSError()
+        guard let requestURL = urlComponents?.url else {
+            print("Request URL is nil")
+            completion(nil)
+            return
+        }
+
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+
+
+
+        URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            if let error = error {
+                print("Error fetching data: \(error)")
+                completion(error)
+                return
+            }
+
+            guard let self = self else {
+                completion(error)
+                return
+            }
+
+            
+
+        }
+
+        .resume()
 
     }
 
 
 
 }
+
+
+
