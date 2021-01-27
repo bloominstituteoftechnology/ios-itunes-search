c277119636a01ffc3be1eabb9b65d2a1b81125e7
James <jameswilliammcdougall@gmail.com>
2021-01-26 21:10:08 -0600

    Initial project commit

---
+++ iTunes Search/iTunes Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by James McDougall on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
