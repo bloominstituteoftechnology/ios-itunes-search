922b9d8c0040019ced3a861533f7afad554b0073
James <jameswilliammcdougall@gmail.com>
2021-01-26 21:45:57 -0600

    Result Type Enum Complete

---
+++ iTunes Search/iTunes Search/Controllers/ModelControllers/ResultType.swift
@@ -0,0 +1,14 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by James McDougall on 1/26/21.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
---
+++ iTunes Search/iTunes Search/Controllers/ModelControllers/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  iTunes Search
+//
+//  Created by James McDougall on 1/26/21.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+    
+    var title: String
+    var creator: String
+}
+
+struct SearchResults {
+    let results: [SearchResult]
+}
